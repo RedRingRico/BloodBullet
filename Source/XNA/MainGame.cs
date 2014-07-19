54855e0c8979abceaae443beba0d0e81ae1052e1
Rico <teh_programerer@interactivelaboratories.com>
2014-07-19 19:33:10 +0100

    Removed unused code
    |SOURCE|
    * Removed Program.cs and Game1.cs, they are superseded by MainGame.cs

---
+++ Source/XNA/MainGame.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BloodBullet
+{
+	static class MainGame
+	{
+		static void Main( string[ ] p_Args )
+		{
+			while( true )
+			{
+			}
+		}
+	}
+}
